74ab0461d166ee9a8c647ba1945ffd084f73f836
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-25 23:34:09 +0200

    Have a sample.

---
+++ KeyBox/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace KeyBox
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnUpExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            dot.SetValue(Canvas.TopProperty, Canvas.GetTop(dot) - 1);
+        }
+
+        private void OnDownExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            dot.SetValue(Canvas.TopProperty, Canvas.GetTop(dot) + 1);
+        }
+    }
+}
